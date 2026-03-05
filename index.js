f987f2233e50390b51d538febce6e83377a623cb
Nurillo1997 <nurilloabdurazzoqov@gmail>
2026-03-05 13:42:21 +0900

    feat(server): setup express server, middlewares and session

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 
+const app = express();
+
 // DB connect
 connectDB();
+
+//Middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+//Flash message
+app.use((req,res,next)=>{
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
+
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+// Server connection
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=> console.log(`Server running on ${PORT}`));
