5583cb57e697954afdb8b1ec9ccb203b8c28b691
Nurillo1997 <nurilloabdurazzoqov@gmail.com>
2026-03-19 12:37:13 +0900

    fix(auth): configure production Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL:
+      process.env.NODE_ENV === "production"
+        ? "https://nurstore.onrender.com/auth/google/callback"
+        : "http://localhost:4000/auth/google/callback",
     },
     async (_, __, profile, done) => {
       try {
